5b74936180863125cc89ca541959fb15eb92b482
Dmgoj <domagoj.grgic@outlook.com>
2024-07-22 19:28:36 +0200

    Add project files.

---
+++ Ispit.Api/Data/TodoListContext.cs
@@ -0,0 +1,13 @@
+﻿using Ispit.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ispit.Api.Data
+{
+    public class TodoListContext : DbContext
+    {
+        public TodoListContext(DbContextOptions<TodoListContext> options) : base(options) { }
+        public virtual DbSet<TodoList> TodoLists { get; set; }
+
+       
+    }
+}
